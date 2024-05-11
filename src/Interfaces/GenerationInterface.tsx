f3e50391efdcae9bcabaa4b27f5f264f54e8bb05
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-05-11 14:50:58 -0400

    Extracted CourseSemesterContainer from GenerationInterface

--- src/Interfaces/GenerationInterface.tsx
+++ src/Interfaces/GenerationInterface.tsx
@@ -1,4 +1,5 @@
 import NavigationBar from "./NavigationBar"
+import CourseSemesterContainer from "./CourseSemesterContainer"
 import "./GenerationInterface.css"
 import React, { type ReactEventHandler } from "react"
 
@@ -60,41 +61,10 @@ function GenerateButton(){
   )
 }
 
-function CareerSemesterSelection() {
-  const [selectedCareer, setSelectedCareer] = React.useState("ingenieria_informatica") // Valor default
-  const [selectedSemester, setSelectedSemester] = React.useState("20240205") // Valor default
-
-  function handleCareerChange(event : any) {
-    setSelectedCareer(event.target.value)
-    // TODO: Add function that uses selectedSemester and event.target.value to fetch horarios
-  }
-
-  const handleSemesterChange = (event : any) => {
-    setSelectedSemester(event.target.value)
-    // TODO: Add function that uses selectedCareer and event.target.value to fetch horarios
-  }
-
-  return (
-    <div>
-      <div className="semester-dropdown-container">
-        <select value={selectedSemester} onChange={handleSemesterChange}>
-          <option value="20240205">2024 Feb-Jun</option>
-        </select> 
-      </div>
-      <div className="career-dropdown-container">
-        <select value={selectedCareer} onChange={handleCareerChange}>
-          <option value="ingenieria_informatica">Ingenieria Informatica</option>
-          <option value="theology">Teologia</option>
-        </select> 
-      </div>
-    </div>
-  )
-}
-
 function CourseBox() {
   return (
     <div className="course-box">      
-        <CareerSemesterSelection/>
+        <CourseSemesterContainer/>
         <CourseContainer/>
         <GenerateButton/>
     </div>
