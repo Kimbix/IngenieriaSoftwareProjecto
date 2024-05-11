76436f176a9dd1a65225f3768a637c6ccb5e59db
HumbertoAlemanOdreman <haleman.23@est.ucab.edu.ve>
2024-05-11 12:01:30 -0400

    Updated NavigationBar.tsx

--- src/Interfaces/NavigationBar.tsx
+++ src/Interfaces/NavigationBar.tsx
@@ -1,10 +1,10 @@
 export default function NavigationBar() {
   return (
     <div className="navbar-container">
-      <h1>HorariosPP</h1>
-      <div><a href="schedule">Mi Horario</a></div>
-      <div><a href="generation">Generar</a></div>
-      <div><a href="template">Agregar Horario</a></div>
+      <div><h1><a href="/">HorariosPP</a></h1></div>
+      <div><a href="/schedule">Mi Horario</a></div>
+      <div><a href="/generation">Generar</a></div>
+      <div><a href="/template">Agregar Horario</a></div>
     </div>
   )
 }
