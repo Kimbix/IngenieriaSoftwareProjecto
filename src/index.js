22c64175e23282839a789f5df96e069edcf4cbf5
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2024-05-13 12:33:28 -0400

    areglarindex.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,37 @@
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals.js";
 
-import React from 'react';
-
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from 'react-router-dom';
-
-import GenerationInterface from './Interfaces/GenerationInterface.tsx';
-import LandingInterface from './Interfaces/LandingInterface.tsx';
-import reportWebVitals from './reportWebVitals.js';
-
-import LandingInterface from './Interfaces/LandingInterface.tsx';
+import LandingInterface from "./Interfaces/LandingInterface.tsx";
 import GenerationInterface from "./Interfaces/GenerationInterface.tsx";
-import TimeBlockInterface from './Interfaces/TimeBlockInterface.tsx';
-import MyScheduleInterface from './Interfaces/MyScheduleInterface.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { connectToDatabase, disconnectFromDatabase } from './controller/ControllerDB.ts';
-
+import TimeBlockInterface from "./Interfaces/TimeBlockInterface.tsx";
+import MyScheduleInterface from "./Interfaces/MyScheduleInterface.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  connectToDatabase,
+  disconnectFromDatabase,
+} from "./controller/ControllerDB.ts";
 
 await connectToDatabase();
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Routes>
-      <Route path="/">
-        <Route index element={<LandingInterface/>}/>
-        <Route path="/generation" element={<GenerationInterface/>}/>
-        <Route path="/time_blocks" element={<TimeBlockInterface/>}/>
-        <Route path="/schedule" element={<MyScheduleInterface/>}/>
-      </Route>
-     </Routes>
+      <Routes>
+        <Route path="/">
+          <Route index element={<LandingInterface />} />
+          <Route path="/generation" element={<GenerationInterface />} />
+          <Route path="/time_blocks" element={<TimeBlockInterface />} />
+          <Route path="/schedule" element={<MyScheduleInterface />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
